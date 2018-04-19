00e67aa1b401e0262d6f94c96f8a802f14b21d28
AlexMin314 <m98stone@gmail.com>
2018-04-19 09:38:07 +0800

    [Delete] roll back to init status

--- src/root.js
+++ src/root.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 
 import { theme } from 'modules'
-import Async from './libs/lazyRoute'
-
-const Lazy = Async({
-  options: {
-    LoadingComponent: () => <div>Loading...</div>
-  },
-  App: {
-    loader: () => import('./containers/App')
-  }
-})
+import App from './App'
 
 const Root = props => {
   return (
     <Provider store={{}}>
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <Route exact path={'/'} component={Lazy.App} />
+          <App />
         </BrowserRouter>
       </ThemeProvider>
     </Provider>
